d47d1c5082f8d3897e603fd08157c6d4db39acae
SumanthRao31 <59040372+SumanthRao31@users.noreply.github.com>
2019-12-20 11:30:06 +0530

    Add files via upload

---
+++ yy.c
@@ -0,0 +1,16 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+   int  a=(10,20,30);
+    printf("%d",a);
+    return 0;
+}
